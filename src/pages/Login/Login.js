d30841446b95e470cd1574bcd98afe882be8c8d9
jacobsanosh <jacobsanosh@gmail.com>
2022-12-07 07:05:01 +0530

    login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+  return (
+    <div className="Login__main_container">
+      <div className="lgn__container">
+        <div className="lgn__left"></div>
+        <div className="lgn__right">
+          <div className="lgn__input">
+          <div className="lgn__inputBox">
+              <h1>Login</h1>
+            </div>
+            <div className="lgn__inputBox">
+              <label>Username</label>
+              <input type="text" placeholder="enter user name" />
+            </div>
+            <div className="lgn__inputBox">
+              <label>Password</label>
+              <input type="password" placeholder="enter password" />
+            </div>
+            <div className="lgn__inputBox">
+              <input type="submit" value="Login" />
+            </div>
+            <p className="lgn__forget">
+              Forgot Password? <a href="/">Click Here</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
