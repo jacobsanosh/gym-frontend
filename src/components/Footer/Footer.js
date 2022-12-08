e9dcada73bd37d2b29fdf9bd7b5dea22e2b09b2b
jacobsanosh <jacobsanosh@gmail.com>
2022-12-09 01:00:12 +0530

    adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 function Footer() {
   return (
-    <div>
-      footer
+    <div className="footer__contaier">
+      <footer className="footer">
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
+
+        <ul className="menu">
+          <li className="menu__item">
+            <a className="menu__link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="/">
+              About us
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="/">
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
