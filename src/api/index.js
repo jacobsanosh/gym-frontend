86132acbe84b082b2a1762648dbaf0895d9fb1dd
jacobsanosh <jacobsanosh@gmail.com>
2022-12-10 23:02:43 +0530

    made some change

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const token = btoa("user1@gmail.com:password");
 const client = axios.create({
-  baseURL: "https://gym-backend-dbms.onrender.com/",
+  baseURL: "http://localhost:5000/",
   headers: {
     Authorization: `Basic ${token}`,
   },
@@ -31,4 +31,55 @@ async function login(email, password, type) {
   return response;
 }
 
-export { getTrainers, login };
+async function registeruser(
+  email,
+  password,
+  age,
+  height,
+  weight,
+  need,
+  profileImageUrl
+) {
+  // console.log("res user")
+  const response = await client.post("/user/register", {
+    name: email.split("@")[0],
+    email,
+    heightInCm: height,
+    password,
+    weightInKg: weight,
+    profileImageUrl,
+    need,
+    age,
+  });
+  console.log("respose at index",response)
+  if (response.status === 200) {
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+  }
+  return response;
+}
+async function registertrainer(
+  email,
+  password,
+  gymname,
+  place,
+  phone,
+  specialization,
+  profileImageUrl
+) {
+  const response = await client.post("/trainer/register", {
+    name: email.split("@")[0],
+    email,
+    password,
+    gymname,
+    place,
+    phone,
+    specialization,
+    profileImageUrl,
+  });
+  if (response.status === 200) {
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+  }
+}
+export { getTrainers, login, registeruser,registertrainer };
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,15 +1,18 @@
 import React from "react";
+
 import "./Register.css";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import MainLayout from "../../components/MainLayout/MainLayout";
+import { registeruser } from "../../api";
+
 function Register() {
   const navigate = useNavigate();
   //creating an useState
   const [usertype, setUsertype] = useState(null);
   const onChangeValue = (event) => {
     setUsertype(event.target.value);
-    console.log(usertype);
+    // console.log(usertype);
   };
   const Trainer = () => {
     if (usertype === "trainer") {
@@ -17,23 +20,23 @@ function Register() {
         <>
           <div className="lgn__inputBox">
             <label>gym name</label>
-            <input type="text" placeholder="enter gym name" />
+            <input type="text" placeholder="enter gym name" onChange={(e)=>setGymname(e.target.value)}/>
           </div>
           <div className="lgn__inputBox">
             <label>place</label>
-            <input type="text" placeholder="enter your place" />
+            <input type="text" placeholder="enter your place" onChange={(e)=>setPlace(e.target.value)}/>
           </div>
           <div className="lgn__inputBox">
             <label>phone number</label>
-            <input type="tel" placeholder="enter phone number" />
+            <input type="tel" placeholder="enter phone number" maxLength={10} pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" onChange={(e)=>setPhone(e.target.value)}/>
           </div>
           <div className="lgn__inputBox">
             <label>specialization</label>
-            <input type="text" placeholder="enter your specialization" />
+            <input type="text" placeholder="enter your specialization" onChange={(e)=>setSpecialization(e.target.value)}/>
           </div>
           <div className="lgn__file">
             <label>Qualification</label>
-            <input type="file" id="file"/>
+            <input type="file" id="file" onChange={(e)=>setQualification(e.target.value)}/>
           </div>
         </>
       );
@@ -42,15 +45,15 @@ function Register() {
         <>
           <div className="lgn__inputBox">
             <label>age</label>
-            <input type="number" placeholder="enter ages" />
+            <input type="number" placeholder="enter ages" onChange={(e)=>setAge(e.target.value)}/>
           </div>
           <div className="lgn__inputBox">
             <label>height</label>
-            <input type="number" placeholder="enter height in cm" />
+            <input type="number" placeholder="enter height in cm" onChange={(e)=>setHeight(e.target.value)}/>
           </div>
           <div className="lgn__inputBox">
             <label>weight</label>
-            <input type="number" placeholder="enter weight in kg" />
+            <input type="number" placeholder="enter weight in kg" onChange={(e)=>setWeight(e.target.value)}/>
           </div>
           <div className="lgn__inputBox"></div>
           <div className="lgn__inputBox">
@@ -59,16 +62,63 @@ function Register() {
               className="reg__textarea"
               rows={5}
               cols={40}
+              onChange={(e)=>setNeed(e.target.value)}
             ></textarea>
           </div>
         </>
       );
     }
   };
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [gymname, setGymname] = useState("")
+  const [place, setPlace] = useState("")
+  const [phone, setPhone] = useState("")
+  const [specialization, setSpecialization] = useState("")
+  const [qualification, setQualification] = useState("")
+  const [age, setAge] = useState("")
+  const [height, setHeight] = useState("")
+  const [weight, setWeight] = useState("")
+  const [need, setNeed] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false);
+  const registerAttempt=async(e)=>{
+    try{
+      
+      setLoading(true)
+      if (usertype === "trainer") {
+        const response=await registeruser(email,password,gymname,place,phone,specialization,qualification);
+        
+        if(response.status===200){
+          navigate('/')
+        }
+      }
+      else{
+        const response=await registeruser(email,password,age,height,weight,need,'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80',need);
+        console.log("the response is ",response)
+        if(response.status===200)
+        {
+          navigate('/')
+        }
+      }
+    }
+    catch(error){
+      console.log(error)
+      if (error.response.status === 409) {
+        setError("user already exists");
+      } else  
+      {
+        setError("something went wrong")
+      }
+    
+    }
+    finally{
+      setLoading(false);
+    }
+  }
   return (
     <MainLayout>
 
-   
     <div className="Login__main_container">
       <div className="lgn__container">
         <div className="lgn__left"></div>
@@ -79,11 +129,11 @@ function Register() {
             </div>
             <div className="lgn__inputBox">
               <label>Username</label>
-              <input type="text" placeholder="enter your name" />
+              <input type="text" placeholder="enter your name" onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div className="lgn__inputBox">
               <label>Password</label>
-              <input type="password" placeholder="enter password" />
+              <input type="password" placeholder="enter password" onChange={(e)=>setPassword(e.target.value)}/>
             </div>
             <div className="lgn__inputBox" onChange={onChangeValue}>
               <input type="radio" value="trainer" name="type_user" /> Trainer
@@ -93,13 +143,18 @@ function Register() {
     
 
             <div className="lgn__inputBox">
-              <input
-                type="submit"
-                value="register"
-                onClick={() => {
-                  navigate("/");
-                }}
-              />
+            <div className="lgn__inputBox">
+                <div className="error">{error}</div>
+                <input
+                  type="submit"
+                  value={loading ? "Loading..." : "register"}
+                  disabled={loading}
+                  onClick={() => {
+                    registerAttempt();
+                  }}
+                />
+              </div>
+            
             </div>
           </div>
         </div>
